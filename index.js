e68c56b3232bda7e9776ed9204eb59aa3202de1c
Subair Oyindamola <hoyendah@live.co.uk>
2016-12-06 14:36:37 +0100

    updating gitignore,

--- index.js
+++ index.js
@@ -30,37 +30,66 @@ const rl = readline.createInterface({
 console.log('******** Enter twitter username below ********')
 
 rl.question('Username: ', (answer) => {
-    client.get('statuses/user_timeline', { q: `${answer}`, count: 20 }, function(error, tweets, response) {
+    console.log('*** Downloading tweets... ***');
+    client.get('statuses/user_timeline', { q: `${answer}`, count: 5 }, function(error, tweets, response) {
         if (!error) {
-            const obj = { tweets: [] };
-            obj.tweets.push(tweets);
+            const obj = { tweets: tweets };
+            // obj.tweets.push(tweets);
             const json = JSON.stringify(obj);
-            // const bar = new ProgressBar('Downloading... [:bar] :percent :etas', {
-            //     complete: green,
-            //     incomplete: red,
-            //     total: 20
-            // });
+
             fs.writeFileSync('tweets.json', json, { space: 4 });
-            console.log(tweets);
-        } else if (error) {
+            console.log('*** Download Complete ***');
+            // console.log(tweets);
+
+        } else {
             console.log('Oh no! error');
         }
     });
 
-    // word count frequency
-    function wordFrequency(s) {
-        s = s.replace(/(^\s*)|(\s*$)/gi, ""); //exclude  start and end white-space
-        s = s.replace(/[ ]{2,}/gi, " "); //2 or more space to 1
-        s = s.replace(/\n\r/, "\n"); //newline
-        s = s.replace(/[^a-zA-Z 0-9]+/g, ''); //special characters
-        const words = s.split(/\s/);
-        const wordMap = {};
-
-    }
+    //text is undefined, cant access text
+    fs.readFile('./tweets.json', 'utf8', function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            obj = JSON.parse(data); //now it an object
+            for (i = 1; i <= obj.tweets.length; i++) {
+                // console.log(wordFrequency(obj.tweets[i].text));
+                console.log(obj.tweets[i].text);
+            }
+        };
+    });
 
     rl.close();
 });
 
+// word count frequency
+function wordFrequency(s) {
+    s = s.replace(/(^\s*)|(\s*$)/gi, ""); //exclude  start and end white-space
+    s = s.replace(/[ ]{2,}/gi, " "); //2 or more space to 1
+    s = s.replace(/\n\r/, "\n"); //newline
+    s = s.replace(/[^a-zA-Z 0-9]+/g, ''); //special characters
+    const words = s.split(/\s/);
+    const wordMap = {};
+    const stop_words = ['that', 'what', 'who', 'than', 'that', 'then'];
+    for (i = 0; i < words.length; i++) {
+        for (j = 0; j < stop_words; j++) {
+            if (Object.hasOwnProperty.call(wordMap, w) && i !== j) {
+                wordMap[w] += 1;
+            } else if (!Object.prototype.hasOwnProperty.call(wordMap, w)) {
+                wordMap[w] = 1;
+            }
+        }
+    }
+    // words.forEach(function(w) {
+    //     //check if a word has its own property
+    //     if (Object.hasOwnProperty.call(wordMap, w)) {
+    //         wordMap[w] += 1;
+    //     } else {
+    //         wordMap[w] = 1;
+    //     }
+    // });
+    return words
+};
 
 
 // var parameters = {
